25066918b66784bd13c10e7faa45d2b3752059a3
Victor Santana <santanav589@gmail.com>
2023-03-02 09:19:02 -0300

    Adicionando alert ao editar

--- resources/views/user_update.blade.php
+++ resources/views/user_update.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,29 +14,31 @@
     <link rel="stylesheet" href="css/estilos_user.css">
     <title>Atualizar usuário</title>
 </head>
+
 <body>
     <section class="container">
         <h1>Atualizar usuário</h1>
 
-        @if(session()->has('success'))
+        @if (session()->has('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
             </div>
         @endif
-        
-        @if(session()->has('error'))
+
+        @if (session()->has('error'))
             <div class="alert alert-danger">
                 {{ session('error') }}
             </div>
         @endif
 
-        <form action="{{route('update.user.post')}}" method="POST">
+        <form action="{{ route('update.user.post') }}" method="POST">
             @csrf
             <h3>Dados pessoais</h3>
             <div class="row">
                 <div class="col-lg-6">
                     <label for="Primeiro nome">Primeiro nome<span class="red">*</span></label>
-                    <input name="primeiro_nome" class="form-control @error('primeiro_nome') is-invalid @enderror" type="text" value="{{$primeiro_nome}}" placeholder="Primeiro nome" required>
+                    <input name="primeiro_nome" class="form-control @error('primeiro_nome') is-invalid @enderror"
+                        type="text" value="{{ $primeiro_nome }}" placeholder="Primeiro nome" required>
 
                     @error('primeiro_nome')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -44,7 +47,8 @@
 
                 <div class="col-lg-6">
                     <label for="Sobrenome">Sobrenome<span class="red">*</span></label>
-                    <input name="sobrenome" class="form-control @error('sobrenome') is-invalid @enderror" type="text" value="{{$sobrenome}}" placeholder="Sobrenome" required>
+                    <input name="sobrenome" class="form-control @error('sobrenome') is-invalid @enderror" type="text"
+                        value="{{ $sobrenome }}" placeholder="Sobrenome" required>
 
                     @error('sobrenome')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -55,7 +59,8 @@
             <div class="row margin-top">
                 <div class="col-lg-6">
                     <label for="Primeiro nome">N° Celular<span class="red">*</span></label>
-                    <input name="num_celular" class="form-control @error('num_celular') is-invalid @enderror" type="text" value="{{$num_celular}}" placeholder="(73) 98189****" required>
+                    <input name="num_celular" class="form-control @error('num_celular') is-invalid @enderror"
+                        type="text" value="{{ $num_celular }}" placeholder="(73) 98189****" required>
 
                     @error('num_celular')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -64,7 +69,8 @@
 
                 <div class="col-lg-6">
                     <label for="Sobrenome">E-mail<span class="red">*</span></label>
-                    <input name="email" class="form-control @error('email') is-invalid @enderror" type="email" value="{{$email}}" placeholder="exemplo@gmail.com" required>
+                    <input name="email" class="form-control @error('email') is-invalid @enderror" type="email"
+                        value="{{ $email }}" placeholder="exemplo@gmail.com" required>
 
                     @error('email')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -76,7 +82,8 @@
             <div class="row">
                 <div class="col-lg-6">
                     <label for="Cargo">Cargo<span class="red">*</span></label>
-                    <input name="cargo" class="form-control @error('cargo') is-invalid @enderror" type="text" value="{{$cargo}}" placeholder="Cargo" required>
+                    <input name="cargo" class="form-control @error('cargo') is-invalid @enderror" type="text"
+                        value="{{ $cargo }}" placeholder="Cargo" required>
 
                     @error('cargo')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -85,18 +92,41 @@
 
                 <div class="col-lg-6">
                     <label for="Departamento">Departamento<span class="red">*</span></label>
-                    <input name="departamento" class="form-control @error('departamento') is-invalid @enderror" type="text" value="{{$departamento}}" placeholder="Departamento" required>
+                    <input name="departamento" class="form-control @error('departamento') is-invalid @enderror"
+                        type="text" value="{{ $departamento }}" placeholder="Departamento" required>
 
                     @error('departamento')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
-                <input class="invisible" name="id" type="number" value="{{$user_id}}" required>
+                <input class="invisible" name="id" type="number" value="{{ $user_id }}" required>
             </div>
-            
-            <button class="btn" type="submit">Atualizar usuário</button>
+
+            <button class="btn" onclick="submitForm(this.form)" type="button">Atualizar usuário</button>
         </form>
     </section>
-    
+
+    <script>
+        function submitForm(form) {
+            submitButton = document.getElementById("formsubmit");
+            const confirmMessage = "Os dados modificados serão persisitidos no banco de dados";
+            swal({
+                title: "Você deseja editar este registro?",
+                text: confirmMessage,
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            }).then((willDelete) => {
+                if (willDelete) {
+                    form.submit();
+                }
+            });
+        } 
+    </script>
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="{{ asset('assets/lib/jquery/jquery.min.js') }}" type="text/javascript"></script>
+
 </body>
+
 </html>
